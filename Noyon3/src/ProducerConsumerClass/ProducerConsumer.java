69bbb93a2fe262c7cd8e9f5b8b2471f58f8a09f5
Md.Noyon Mahmud <noyonmahmud042@gmail.com>
2024-10-15 09:49:28 +0600

    Producer Consumer

--- Noyon3/src/ProducerConsumerClass/ProducerConsumer.java
+++ Noyon3/src/ProducerConsumerClass/ProducerConsumer.java
@@ -6,32 +6,45 @@ import java.util.Queue;
 public class ProducerConsumer {
 
 		private final int LIMIT = 10;
+		
 		private final Queue<Integer> queue = new LinkedList<>();
+		
 		public void produce() throws InterruptedException {
+			
 		int value = 0;
-		while (true) {
+		
+		while (true) {			
 		synchronized (this) {
+			
 		while (queue.size() == LIMIT) {
 		wait();
 		}
+		
 		queue.add(value);
 		System.out.println("Produced " + value);
 		value++;
 		notify();
 		Thread.sleep(100);
+		
 		}
 		}
 		}
+		
+		
 		public void consume() throws InterruptedException {
+			
 		while (true) {
 		synchronized (this) {
 		while (queue.isEmpty()) {
 		wait();
 		}
+		
 		int value = queue.poll();
+		
 		System.out.println("Consumed " + value);
 		notify();
 		Thread.sleep(1000);
+		
 		}
 		}
 		}
--- Noyon3/src/ProducerConsumerClass/ProducerConsumerDemo.java
+++ Noyon3/src/ProducerConsumerClass/ProducerConsumerDemo.java
@@ -3,22 +3,35 @@ package ProducerConsumerClass;
 public class ProducerConsumerDemo {
 
 	public static void main(String[] args) {
+		
+		
 		ProducerConsumer pc = new ProducerConsumer();
 		
-		Thread producerThread = new Thread(() -> {
-			try {
+		Thread producerThread = new Thread(() -> {			
+			try
+			{				
 			pc.produce();
-			} catch (InterruptedException e) {
-			e.printStackTrace();
+			} 
+			
+			catch (InterruptedException e) {				
+			e.printStackTrace();			
 			}
 			});
+		
+		
+		
 			Thread consumerThread = new Thread(() -> {
-			try {
-			pc.consume();
-			} catch (InterruptedException e) {
+				
+			try {				
+			pc.consume();			
+			} 
+			
+			catch (InterruptedException e) {
 			e.printStackTrace();
 			}
+			
 			});
+			
 			producerThread.start();
 			consumerThread.start();
 			}
